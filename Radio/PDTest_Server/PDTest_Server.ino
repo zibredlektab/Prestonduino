64bdc519f6665b140c391098715f1a05245078ef
Zib Redlektab <zibredlektab@gmail.com>
2021-06-27 19:12:19 -0400

    Added switch reading to PDServer, untested

--- Radio/PDTest_Server/PDTest_Server.ino
+++ Radio/PDTest_Server/PDTest_Server.ino
@@ -1,11 +1,32 @@
 #include <PDServer.h>
 
+#define PIN1 5
+#define PIN2 6
+#define PIN4 7
+#define PIN8 8
+
 PDServer *pd;
 
 void setup() {
-  pd = new PDServer(0xA, Serial1);
+  pd = new PDServer(readSwitch(), Serial1);
+  
+  pinMode(PIN1, INPUT_PULLUP);
+  pinMode(PIN2, INPUT_PULLUP);
+  pinMode(PIN4, INPUT_PULLUP);
+  pinMode(PIN8, INPUT_PULLUP);
 }
 
 void loop() {
   pd->onLoop();
+}
+
+uint8_t readSwitch() {
+  uint8_t value = 0;
+  for (int i = 0; i < 4; i++) {
+    if (!digitalRead(i + PIN1)) {
+      value += 1 << i;
+    }
+  }
+
+  return value;
 }
