eeb92cb68de0809f9cff46930aad01bc27bd6aeb
Zib Redlektab <zibredlektab@gmail.com>
2022-10-28 12:13:17 -0400

    More up-to-date testing script

--- Preston_RS232/Preston_RS232.ino
+++ Preston_RS232/Preston_RS232.ino
@@ -1,89 +1,36 @@
-
-#include "PrestonPacket.h"
 #include "PrestonDuino.h"
-
-
-#include <U8g2lib.h>
-
-#include "wiring_private.h" // pinPeripheral() function
-Uart Serial2 (&sercom1, 12, 10, SERCOM_RX_PAD_3, UART_TX_PAD_2);
-void SERCOM1_Handler()
-{
-  Serial2.IrqHandler();
-}
-
-#define SAMPLES 100
 
 PrestonDuino *mdr;
 
-//U8G2_SSD1306_128X64_NONAME_2_HW_I2C oled (U8G2_R0);
-
-//U8G2_SSD1322_NHD_128X64_F_3W_HW_SPI oled(U8G2_R3, 0, 1);
-
-int count;
+long unsigned int time = 0;
+uint8_t i = 0;
 
 void setup() {
 
   Serial.begin(115200);
-  while(!Serial) {};
-  
-  
+  while(!Serial);
 
-
-
- // oled.begin();
-  mdr = new PrestonDuino(Serial2);
-
-  
-  mdr->setMDRTimeout(1000);
-  
-  delay(1000);
-
-  mdr->mode(0x10, 0x3);
-  //byte lensdata[] = {0x2, 0xFF, 0xFF};
-  //mdr->data(lensdata, 3);
+  mdr = new PrestonDuino(Serial1);
+  //mdr->mode(0x01,0x01);
 }
 
-unsigned long focustotal = 0;
 void loop() {
-  mdr->data(0x02);
-  /*uint16_t focus = 0;
-  if (count++ < SAMPLES) {
-    focustotal += analogRead(A1);
-  } else {
-    focus = focustotal/SAMPLES;
-    focustotal = 0;
-    focus = map(focus, 0, 1023, 0, 0xFFFF);
-    count = 0;
-    Serial.print("focus is 0x");
-    Serial.println(focus, HEX);
-    uint8_t focusmin = focus % 0xFF00;
-    uint8_t focusmaj = (focus - focusmin) / 0x100;
-    Serial.print("0x");
-    Serial.print(focusmaj, HEX);
-    Serial.print(" 0x");
-    Serial.println(focusmin, HEX);
-    byte lensdata[] = {0x2, focusmaj, focusmin};
-    mdr->data(lensdata, 3);
+  mdr->onLoop();
+
+  if (time + 100 < millis()) {
+
+    Serial.print("iris is: ");
+    Serial.println(mdr->getIris());
+    time = millis();
+    uint16_t iris = map(time, 0, 10000, 0, 0xFFFF); // full lens rack in 10 seconds...nice and smooth
+    byte irish = iris >> 8;
+    byte irisl = iris & 0xFF;
+    byte irisdata[3] = {0x1, irish, irisl};
+    //mdr->data(irisdata, 3);
   }
-  char* modedesc = "Focus:";
-  //int readout = mdr->getFocusDistance();
 
-  /*
-  oled.clearBuffer();
-  
-  oled.setFont(u8g2_font_logisoso18_tf);
-  //command_reply lensdata = mdr->data(mode);
+  if (millis() > 10000) {
+    while(1);
+  }
 
-  oled.setCursor(11,20);
-  oled.print(modedesc);
-  
-  oled.setCursor(11, 60);
-
-  oled.print(readout);
-
-
-
-  oled.sendBuffer();*/
-  
 }
