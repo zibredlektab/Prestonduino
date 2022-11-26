bb11cb3c69d718d613337551c8c9d4c27ffe5eaf
Zib Redlektab <zibredlektab@gmail.com>
2022-11-26 11:02:35 -0500

    added a shutdown after 8 sec to help with debugging

--- Preston_RS232/Preston_RS232.ino
+++ Preston_RS232/Preston_RS232.ino
@@ -5,6 +5,7 @@ PrestonDuino *mdr;
 long unsigned int time = 0;
 long unsigned int time1 = 0;
 uint8_t i = 0;
+bool continuelooping = true;
 
 void setup() {
 
@@ -21,35 +22,44 @@ void setup() {
 }
 
 void loop() {
-  mdr->onLoop();
+  if (continuelooping) {
+    mdr->onLoop();
 
-  if (time1 + 1000 < millis()) {
-    Serial.println("time to print lens info");
-    time1 = millis();
-    Serial.print("iris: ");
-    Serial.print(mdr->getIris());
-    Serial.print(" focus: ");
-    Serial.print(mdr->getFocus());
-    Serial.print(" zoom: ");
-    Serial.println(mdr->getZoom());
-    mdr->info(1);
-    Serial.println(mdr->getLensName());
+    if (time1 + 1000 < millis()) {
+      Serial.println("time to print lens info");
+      time1 = millis();
+      Serial.print("iris: ");
+      Serial.print(mdr->getIris());
+      Serial.print(" focus: ");
+      Serial.print(mdr->getFocus());
+      Serial.print(" zoom: ");
+      Serial.println(mdr->getZoom());
+      mdr->info(1);
+      Serial.println(mdr->getLensName());
+    }
+  /*
+    if (time + 6 < millis()) {
+
+      time = millis();
+      //command_reply lens = mdr->info(1);
+      //Serial.print("current lens name is: ");
+      //Serial.println(mdr->getLensName());
+      Serial.println("time to update aux");
+
+      uint16_t iris = map(time, 0, 10000, 0, 0xFFFF); // full lens rack in 10 seconds...nice and smooth
+      byte irish = iris >> 8;
+      byte irisl = iris & 0xFF;
+      byte irisdata[3] = {0x8, irish, irisl};
+      mdr->data(irisdata, 3);
+    }*/
+
+    if (time + 8000 < millis()) {
+      mdr->mode(0,0);
+      delay(500);
+      continuelooping = false;
+      Serial.println("Debug time's up, shutting down.");
+    }
   }
-/*
-  if (time + 6 < millis()) {
-
-    time = millis();
-    //command_reply lens = mdr->info(1);
-    //Serial.print("current lens name is: ");
-    //Serial.println(mdr->getLensName());
-    Serial.println("time to update aux");
-
-    uint16_t iris = map(time, 0, 10000, 0, 0xFFFF); // full lens rack in 10 seconds...nice and smooth
-    byte irish = iris >> 8;
-    byte irisl = iris & 0xFF;
-    byte irisdata[3] = {0x8, irish, irisl};
-    mdr->data(irisdata, 3);
-  }*/
 
   
 }
