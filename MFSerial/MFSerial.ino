0c2161089e71d2c66f9396865613de6bf73f83e0
Zib Redlektab <zibredlektab@gmail.com>
2023-07-24 18:26:26 -0400

    zooming is working, at least when testing with a pot

--- MFSerial/MFSerial.ino
+++ MFSerial/MFSerial.ino
@@ -2,14 +2,18 @@
 
 #define REPEAT_DELAY 10
 #define SETBUTTONPIN 5
-#define MFPIN 6
+#define MFPIN A0
 #define LEDPIN 13
+#define MESSAGE_DELAY 12
 
+bool firstrun = true;
 PrestonDuino *mdr;
 int mfoutput = 0; // current velocity specified by MicroForce
 uint16_t curzoom; // current zoom position
 bool setpressed = false; // is the "Set" button depressed?
 uint32_t timesetpressed; // when was the "Set" button depressed?
+uint32_t timelastsent = 0; // when was the last mdr message sent?
+int count; // number of microforce samples taken in this averaging
 
 bool limits = false; // is the zoom currently limited
 uint16_t firstlimit; // initial position when set button is first pressed
@@ -19,7 +23,13 @@ uint16_t tightlimit = 0xFFFF; // encoder position of tight zoom limit
 
 
 void setup() {
+  Serial.begin(9600);
+  while(!Serial){};
+  Serial.println("---Start---");
+
   mdr = new PrestonDuino(Serial1);
+  delay(100);
+
   pinMode(SETBUTTONPIN, INPUT_PULLUP);
   pinMode(MFPIN, INPUT);
   pinMode(LEDPIN, OUTPUT);
@@ -27,36 +37,68 @@ void setup() {
   mdr->shutUp();
   mdr->mode(0x9, 0x4); // commanded motor positions, zoom position, streaming, controlling zoom
   mdr->data(0x44); // request only zoom position
+  byte zoomdata[3] = {0x3, 0x7F, 0xFF}; // establish a known starting position, halfway through range
+  curzoom = 0x7FFF;
+  mdr->data(zoomdata, 3);
+
+  timelastsent = millis();
 }
 
 void loop() {
   mdr->onLoop();
-  curzoom = mdr->getZoom();
-  uint16_t newzoom;
 
-  // determine step size by microforce output
-  mfoutput = getMFOutput();
-  int stepsize = mfoutput;
-
-  if (mfoutput < 0) {
-    if (curzoom + stepsize >= widelimit) {
-      newzoom = curzoom + stepsize;
-    } else {
-      newzoom = widelimit;
-    }
-  } else if (mfoutput > 0) {
-    if (curzoom + stepsize <= tightlimit) {
-      newzoom = curzoom + stepsize;
-    } else {
-      newzoom = tightlimit;
-    }
+  if (firstrun && timelastsent + 1000 > millis()) { // give the mdr a little time to get caught up at the beginning
+    return;
   }
 
-  byte zoomdata[3] = {0x4, highByte(newzoom), lowByte(newzoom)};
+  firstrun = false;
 
-  if (newzoom != curzoom) mdr->data(zoomdata, 3); // send to mdr
+  if (count == 0 || timelastsent + MESSAGE_DELAY > millis()) { // average input from microforce until message delay period is reached
+    mfoutput += getMFOutput();
+    count++;
+  } else {
+    mfoutput /= count;
+    count = 0;
 
+    uint16_t newzoom = curzoom;
+    //curzoom = mdr->getZoom();
+    
+    Serial.print("Current zoom position is 0x");
+    Serial.print(curzoom, HEX);
 
+    int stepsize = mfoutput; // eventually this will be scaled for soft stops
+    
+    Serial.print(", step size is ");
+    Serial.print(stepsize);
+
+    if (mfoutput < 0) { // zooming out
+      if (curzoom + stepsize >= widelimit) {
+        newzoom = curzoom + stepsize;
+      } else {
+        newzoom = widelimit;
+      }
+    } else if (mfoutput > 0) { // zooming in
+      if (curzoom + stepsize <= tightlimit) {
+        newzoom = curzoom + stepsize;
+      } else {
+        newzoom = tightlimit;
+      }
+    }
+
+    Serial.print(", new zoom should be 0x");
+    Serial.println(newzoom, HEX);
+
+    byte zoomdata[3] = {0x4, highByte(newzoom), lowByte(newzoom)};
+
+    if (newzoom != curzoom) {
+      mdr->data(zoomdata, 3); // send to mdr
+      curzoom = newzoom;
+    }
+
+    timelastsent = millis();
+    mfoutput = 0;
+  }
+/*
   // check if set button is pressed
   if (!digitalRead(SETBUTTONPIN)) { // set button is pressed
     if (!setpressed) {
@@ -91,7 +133,7 @@ void loop() {
     digitalWrite(LEDPIN, HIGH);
   } else {
     digitalWrite(LEDPIN, LOW);
-  }
+  }*/
 }
 
 int getMFOutput() {
